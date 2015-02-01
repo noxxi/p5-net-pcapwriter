234c9b2deabd797f96a86a5b236a6e3d30fd54ce
Ken Crowell <oeuftete@gmail.com>
2015-02-01 12:49:10 -0400

    Add LICENSE

--- lib/Net/PcapWriter.pm
+++ lib/Net/PcapWriter.pm
@@ -230,3 +230,10 @@ sequence $seq and data $data.
 =head1 AUTHOR
 
 Steffen Ullrich <sullr@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Steffen Ullrich.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
